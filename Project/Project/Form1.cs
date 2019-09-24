2fc1f1ca542a0451a75c9d9c5ea08b75a679b2f1
ict301 <ict301@ict301.com>
2019-09-24 08:45:06 +0600

    реализовал метод нахождения обратной матрицы

--- Project/Project/Form1.cs
+++ Project/Project/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MathNet.Numerics.LinearAlgebra.Double;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,37 @@ namespace Project
         public Form1()
         {
             InitializeComponent();
+
+            var matrix = DenseMatrix.OfArray(new double[,]
+            {
+                { 17,17,5},
+                {21,18,21 },
+                {2,2,19}
+            });
+
+            textBox1.Text = matrix.ToMatrixString();
+
+            textBox1.Text = textBox1.Text + "\ndet: " + Math.Abs(matrix.Determinant()) % 26;
+
+            textBox2.Text = Inverse(matrix).ToMatrixString();
+
+        }
+
+        private Matrix Inverse(Matrix matrix)
+        {
+            var outputMatrix = DenseMatrix.Build.DenseDiagonal(matrix.RowCount, matrix.ColumnCount, 0);
+            var matrixDet = (int) matrix.Determinant() % 26;
+            for (int i = 0; i < matrix.RowCount; i++)
+            {
+                for (int j = 0; j < matrix.ColumnCount; j++)
+                {
+                    var tempMatrix = matrix.RemoveRow(i).RemoveColumn(j);
+                    var insertedValue = Math.Pow(-1, i + j) * tempMatrix.Determinant();
+                    outputMatrix.At(j, i, Math.Round(insertedValue / matrixDet));
+                }
+            }
+
+            return (Matrix)outputMatrix;
         }
     }
 }
